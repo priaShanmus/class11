e4fa9e4ee108fabc34874ea5b9d9703088f30bb7
priaShanmus <92869700+priaShanmus@users.noreply.github.com>
2022-04-16 20:07:02 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
 var trex
 var trexRunning
-
+var edges
+var ground
 function preload(){
   trexRunning = loadAnimation("trex1.png","trex2.png","trex4.png")
 }
@@ -13,11 +14,21 @@ function setup(){
 trex = createSprite(50,150,20,20);
 trex.addAnimation("running",trexRunning)
 trex.scale  = 0.5
+
+edges = createEdgeSprites();
+ground = createSprite(50,190,100,20)
 }
 
 
 
 function draw(){
   background("lightBlue");
+
+if(keyDown("space")){
+  trex.velocityY = -6
+}
+
+trex.velocityY = trex.velocityY + 0.5
+trex.collide(ground)
   drawSprites();
 }
